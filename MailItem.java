1a5fa78ae99eeef2601782c948ace07417d5f85d
jorgevff <jvalladaresfontano@gmail.com>
2016-11-18 14:06:01 +0100

    se añade el atributo subject que se imprime con el mensaje

--- MailItem.java
+++ MailItem.java
@@ -13,6 +13,8 @@ public class MailItem
     private String to;
     // The text of the message.
     private String message;
+    
+    private String subject;
 
     /**
      * Create a mail item from sender to the given recipient,
@@ -21,10 +23,11 @@ public class MailItem
      * @param to The intended recipient of this item.
      * @param message The text of the message to be sent.
      */
-    public MailItem(String from, String to, String message)
+    public MailItem(String from, String to, String subject, String message)
     {
         this.from = from;
         this.to = to;
+        this.subject = subject;
         this.message = message;
     }
 
@@ -44,6 +47,10 @@ public class MailItem
         return to;
     }
 
+     public String getSubject()
+    {
+        return subject;
+    }
     /**
      * @return The text of the message.
      */
@@ -59,6 +66,7 @@ public class MailItem
     {
         System.out.println("From: " + from);
         System.out.println("To: " + to);
+        System.out.println("Subject: " + subject);
         System.out.println("Message: " + message);
     }
 }
